18ed08eb623e1b9639cf2908da38c53a9d24853e
Kai Kramer <kai@iMac.localdomain>
2017-10-31 21:26:12 +0100

    support string write

--- km200mqtt.js
+++ km200mqtt.js
@@ -6,19 +6,19 @@ var buffertrim = require('buffertrim');
 var mqtt = require('mqtt');
 require('require-yaml');
 
+// Load config from path in km200_config
 console.log('Starting km200 to mqtt');
-console.log(process.env.km200_config);
+if (process.env.km200config == null) {
+  console.error('Missing enviroment variable km200config');
+  process.exit(-1);
+}
+console.log('Config file: ' + process.env.km200config);
 var config = require(process.env.km200config);
 console.log(config);
-
 var key = Buffer.from(config.km200.key, 'hex');
-var km200host = config.km200.host;
-
-console.log('Connect mqtt: ' + config.mqtt.server);
-var mqttCon = mqtt.connect(config.mqtt.server);
-
 var desEcb = new MCrypt('rijndael-128', 'ecb');
 desEcb.open(key);
+var km200host = config.km200.host;
 
 var writables = {};
 
@@ -31,18 +31,59 @@ function mnemonizeWritable (result) {
         console.log('Writable: ' + result.id + ' (' + result.type + '): ' + result.minValue + ' - ' + result.maxValue);
       }
       writables[result.id] = {
-        type: result.type,
+        valueType: result.type,
         minValue: result.minValue,
         maxValue: result.maxValue,
-        allowedValues: result.type
+        allowedValues: result.allowedValues
       };
     }
   }
 }
 
-function getKM200 (host, measurement, done) {
+console.log('Connect mqtt: ' + config.mqtt.server);
+var mqttCon = mqtt.connect(config.mqtt.server);
+
+mqttCon.on('connect', () => {
+  mqttCon.subscribe('km200/set/#');
+});
+
+mqttCon.on('message', (topic, message) => {
+  if (topic.startsWith('km200/set/')) {
+    let url = topic.substring(9);
+    let value = message.toString();
+    let writable = writables[url];
+    if (writable != null) {
+      if (writable.valueType === 'stringValue' && writable.allowedValues.indexOf(value) !== -1) {
+        console.log('WRITE: ' + value);
+        const postValue = desEcb.encrypt(JSON.stringify({
+          value: value
+        })).toString('base64');
+        var options = {
+          url: 'http://' + km200host + url,
+          body: postValue,
+          headers: {
+            'Content-type': 'application/json',
+            'User-Agent': 'TeleHeater/2.2.3'
+          }
+        };
+        request.post(options, function (error, response, body) {
+          if (!error) {
+            getKM200(url, function done () {});
+          }
+          console.log(error, response.statusCode);
+        });
+      } else {
+        console.log('Invalid valiue: ' + value);
+      }
+    } else {
+      console.log(url + ' not writavle!');
+    }
+  }
+});
+
+function getKM200( url, done) {
   var options = {
-    url: 'http://' + host + measurement.url,
+    url: 'http://' + km200host + url,
     headers: {
       'Content-type': 'application/json',
       'User-Agent': 'TeleHeater/2.2.3'
@@ -68,15 +109,17 @@ function getKM200 (host, measurement, done) {
   });
 }
 
-function checkKM200 () {
+function checkKM200() {
   async.eachSeries(config.measurements,
     function (measurement, cb) {
-      getKM200(km200host, measurement, function (done) {
+      getKM200(measurement.url, function (done) {
         cb(done);
       });
     },
     function (err, result) {
-      console.log(err);
+      if (err) {
+        console.error(err);
+      }
     }
   );
 }
