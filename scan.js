fd16f2758406642f15ee77e2b9fab0a9d3b4bf74
Kai Kramer <kai@iMac.localdomain>
2017-11-04 14:33:45 +0100

    update readme

--- scan.js
+++ scan.js
@@ -6,12 +6,10 @@ var buffertrim = require('buffertrim');
 require('require-yaml');
 var config = require('./config.yml');
 
-console.error(__dirname + '/config.yml');
-console.error(config);
 
 var Table = require('cli-table');
 var table = new Table({
-  head: ['id', 'type', 'value','unit','Rec','Write','min','max','values'],
+  head: ['id', 'type', 'value', 'unit', 'Rec', 'Write', 'min', 'max', 'values'],
   colWidths: [60, 16, 25, 10, 6, 6, 6, 12, 80]
 });
 
@@ -44,7 +42,6 @@ function getKM200 (host, api, done) {
       try {
         var bodyBuffer = Buffer.from(body, 'base64');
         var dataBuffer = buffertrim.trimEnd(desEcb.decrypt(bodyBuffer, 'base64'));
-        // console.log(api,dataBuffer.toString())
         var result = JSON.parse(dataBuffer.toString());
         done(dataBuffer.toString(), result);
       } catch (e) {
@@ -60,29 +57,28 @@ function callKM200 (host, APIs) {
   var API = APIs.shift();
   getKM200(host, API, function (result, json) {
     console.error(API, APIs.length);
-    if (json !== undefined && json.type!=='refEnum') {
-      var entry = [
-        json.id ? json.id :'',
-        json.type ? json.type :'',
-        json.value ? json.value :'',
-        json.unitOfMeasure ? json.unitOfMeasure :'',
-        json.recordable ? json.recordable :'',
-        json.writeable ? json.writeable :'',
-        json.minValue ? json.minValue :'',
-        json.maxValue ? json.maxValue :'',
-        json.allowedValues ? JSON.stringify(json.allowedValues) : ''
-      ];
-      table.push(entry);
+    if (json !== undefined) {
+      if (json.type === 'refEnum') {
+        json.references.forEach(function (e) {
+          APIs.unshift(e.id);
+        });
+      } /*else if (json.value === undefined) {
+        console.error(json);
+      }*/ else {
+        var entry = [
+          json.id ? json.id : '',
+          json.type ? json.type : '',
+          json.value ? json.value : (json.values ? JSON.stringify(json.values) : ''),
+          json.unitOfMeasure ? json.unitOfMeasure : '',
+          json.recordable ? json.recordable : '',
+          json.writeable ? json.writeable : '',
+          json.minValue ? json.minValue : '',
+          json.maxValue ? json.maxValue : '',
+          json.allowedValues ? JSON.stringify(json.allowedValues) : ''
+        ];
+        table.push(entry);
+      }
     }
-    if (json !== undefined && json.type!=='refEnum' && json.value===undefined) {
-      console.error(json);
-    }
-    if (json && json.hasOwnProperty('type') && json.type == 'refEnum') {
-      json.references.forEach(function (e) {
-        APIs.unshift(e.id);
-      });
-    }
-
     if (APIs.length) {
       callKM200(host, APIs);
     } else {
